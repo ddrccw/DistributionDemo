5c80ed41e1165a6ee0afa9d6d81284a432cbde99
ddrccw <ddrccw@gmail.com>
2014-04-13 15:56:44 +0800

    Initial Commit

---
+++ DistributionDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DistributionDemo
+//
+//  Created by ddrccw on 14-4-13.
+//  Copyright (c) 2014年 ddrccw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DistributionDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DistributionDemo
+//
+//  Created by ddrccw on 14-4-13.
+//  Copyright (c) 2014年 ddrccw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
